a7dda2c8e616c1478e4035d78fc03a5bc381328e
Karthikan Jeyabalan <karthi-kan@hotmail.com>
2020-11-06 02:10:21 -0500

    pseudo code - first attempt

---
+++ puzzle.py
@@ -0,0 +1,39 @@
+
+class Move:
+
+# puzzle dimension must be scalable
+
+# state = seqence of number (ex: 3, 0, 1, 4, 2, 6, 5, 7)
+
+# look posibilities(pass the state)
+# use numpy matrix to represnt state
+
+#   find zero
+#       isCorner?
+#           [rXc] = [2x4]
+#           [0,0] is corner
+#           [r-1, 0] is corner [1, 0]
+#           [0, c-1] is corner [0,3]
+#           [r-1,c-1] is corner [1, 3]
+#       rest is all normal moves 
+#       
+#       yes: all wrap
+#       Check position and get posibile moves
+
+
+# 2.4 Analysis
+
+
+
+
+# goal function
+
+# board states
+
+# moves
+
+# positions
+
+# search functions
+
+# input output
