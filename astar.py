4a73ad3c8029efeb46c56ee3027c1a2ece385560
Karthikan Jeyabalan <karthi-kan@hotmail.com>
2020-11-08 21:29:37 -0500

    Fix diagonal move

--- astar.py
+++ astar.py
@@ -16,18 +16,29 @@ class Astar:
         # intitalize lists
         np.array(self.openList,dtype=Puzzle)
         np.array(self.closedList,dtype=Puzzle)
-        self.goal1 = Puzzle(input='1 2 3 4 5 6 7 0', cost=0, f=0)
-        self.goal2 = Puzzle(input='1 3 5 7 2 4 6 0', cost=0, f=0)
+        self.goal1 = Puzzle(input='1 2 3 4 5 6 7 0', g=0, f=0)
+        self.goal2 = Puzzle(input='1 3 5 7 2 4 6 0', g=0, f=0)
         
         self.initial = initial
         self.openList.append(self.initial)
 
+        a = Puzzle(input='0 2 3 1 5 6 7 4', g=0, f=2)
+        a.print()
+        for c in a.getMoves():
+            c.print()
+        self.openList.append(Puzzle(input='0 2 3 1 5 6 7 4', g=0, f=2))
 
+        # i = 0
         while len(self.openList) != 0:
             # sort open list
             self.openList.sort(key=lambda x:x.f)
+            for c in self.openList:
+                c.print()
+
             # get first item
             currentNode = self.openList.pop()
+            print('CurrentNode: ')
+            # currentNode.print()
             self.closedList.append(currentNode);
 
             # compare with goal
@@ -41,23 +52,40 @@ class Astar:
 
             children = currentNode.getMoves()
             for child in children:
-                
+
                 print(child.puzzle)
+
                 # check if child in closedList
-                # if exist skip use continue
+                if any((x.puzzle == child.puzzle).all() for x in self.closedList):
+                    # if exist skip the rest of the loop
+                    continue
 
-                # get f, g, h of child
+                
+                print(str(currentNode.g) +" "+ str(child.g))
                 # g = currentNode.g + distance between child and current
+                child.g = currentNode.g + child.g
                 # h = huristic function
+                child.h = 0
                 # f = g+h
+                child.f = child.g + child.h
 
-                # if child in openList
+                # check if child in openList
+                if any((x.puzzle == child.puzzle).all() for x in self.openList):
+                    # check if child.g is higher then openlist g 
+                    openListNode = np.where(self.openList == child.puzzle)
+                    print(openListNode)
+                    continue
+
                 #   if child.g is higher than the openList node.g
                         # continue 
 
                 # add child to openlist
             # print(children)
             break;
+            i = i+1
+            print("Epoch :" + str(i))
+            if i == 2:
+                break;
         # sort the list
 
 
@@ -81,7 +109,7 @@ class Astar:
 input = "4 2 3 1 5 6 7 0"
 # input = '1 3 5 7 2 4 6 0'
 
-puzzle = Puzzle(input=input, cost=0, f=0)
+puzzle = Puzzle(input=input, g=0, f=0)
 # puzzle.print()
 # puzzle.getMoves()
 
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from puzzle import Puzzle
 from astar import Astar
 
-input = "4 2 3 1 5 6 7 0"
+input = "0 2 3 1 5 6 7 4"
 
-puzzle = Puzzle(input=input, cost=0, f=0)
-# puzzle.print()
-# puzzle.getMoves()
+puzzle = Puzzle(input=input, g=0, f=0)
+
 
 a = Astar(puzzle)
 # print(len(a.openList))
